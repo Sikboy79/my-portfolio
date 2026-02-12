b5bfbcd36e4234489fd7345b349b1f41c7b27c0e
Sikboy79 <custombasewelds@yahoo.com>
2026-02-12 09:54:28 -0700

    testing

--- api/contact/route.ts
+++ api/contact/route.ts
@@ -1,41 +1,41 @@
 
 import nodemailer from "nodemailer";
 
-export async function POST() {
-  console.log("API HIT");
-  return Response.json({ ok: true });
-}
-
-
-// export async function POST(req: Request) {
-//   try {
-//     console.log("Contact API hit");
-
-//     const body = await req.json();
-//     console.log(body);
-
-//     const transporter = nodemailer.createTransport({
-//       host: process.env.SMTP_HOST,
-//       port: Number(process.env.SMTP_PORT),
-//       secure: process.env.SMTP_SECURE === "true",
-//       auth: {
-//         user: process.env.SMTP_USER,
-//         pass: process.env.SMTP_PASS,
-//       },
-//     });
-
-//     await transporter.sendMail({
-//       from: `"Portfolio" <${process.env.SMTP_USER}>`,
-//       to: process.env.CONTACT_EMAIL,
-//       subject: "New message",
-//       text: body.message,
-//     });
-
-//     console.log("Email sent");
-
-//     return Response.json({ ok: true });
-//   } catch (err) {
-//     console.error("EMAIL ERROR:", err);
-//     return Response.json({ ok: false }, { status: 500 });
-//   }
+// export async function POST() {
+//   console.log("API HIT");
+//   return Response.json({ ok: true });
 // }
+
+
+export async function POST(req: Request) {
+  try {
+    console.log("Contact API hit");
+
+    const body = await req.json();
+    console.log(body);
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: process.env.SMTP_SECURE === "true",
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Portfolio" <${process.env.SMTP_USER}>`,
+      to: process.env.CONTACT_EMAIL,
+      subject: "New message",
+      text: body.message,
+    });
+
+    console.log("Email sent");
+
+    return Response.json({ ok: true });
+  } catch (err) {
+    console.error("EMAIL ERROR:", err);
+    return Response.json({ ok: false }, { status: 500 });
+  }
+}
