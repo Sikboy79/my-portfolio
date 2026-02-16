65c54b5993a0fe4979ac7448f66b906b7f48bee4
Sikboy79 <custombasewelds@yahoo.com>
2026-02-16 10:05:54 -0700

    2

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -32,7 +32,7 @@ const projectList = [
   },
   {
     title: "Summarist Library",
-    slug: "ultraverse",
+    slug: "summarist",
     overview:
       "A library app that summarizes content and integrates Stripe for payments.",
     tech: ["NextJS", "Tailwind", "Typescript", "API", "Stripe"] as TechKey[],
--- app/components/UI/ProjectCard.tsx
+++ app/components/UI/ProjectCard.tsx
@@ -39,7 +39,6 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           src={imgSrc}
           alt={title}
           fill
-          unoptimized 
           sizes="(max-width: 768px) 100vw, 33vw"
           className={`object-cover transition-all duration-500 ${
             isHovered ? "opacity-0 scale-105" : "opacity-100 scale-100"
