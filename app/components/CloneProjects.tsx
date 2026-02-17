6435217768d923adb4e8aa5ff9499087f6907ff4
Sikboy79 <custombasewelds@yahoo.com>
2026-02-17 14:24:34 -0700

    netflix live link

--- app/components/CloneProjects.tsx
+++ app/components/CloneProjects.tsx
@@ -33,11 +33,11 @@ const projectList = [
   },
   {
     title: "Netflix Clone",
-    slug: "netflix",
+    slug: "netflixclone",
     overview:
       "Recreated Netflix UI with responsive grids, navigation, and media cards using React and Tailwind.",
     tech: (["React", "Tailwind"] as TechKey[]).map((t) => techIconsMap[t]),
-    link: "",
+    link: "https://sikboy79.github.io/Movie-clone-fullstack/#/",
     codeLink: "https://github.com/Sikboy79/Netflix-clone-fullstack",
   },
 ];
